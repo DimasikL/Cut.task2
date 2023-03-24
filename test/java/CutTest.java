6b98fa14d1c9b1be997db79a0cc41727b63e0ad3
DimasikL <112756852+DimasikL@users.noreply.github.com>
2023-03-24 09:14:00 +0300

    task 2 with problems

---
+++ test/java/CutTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class cutTest {
+    @Test
+    public void testCutByChar() throws IOException {
+        String[] args = {"-c", "-o", "output.txt", "5-14", "input.txt"};
+        cut.main(args);
+        BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+        String line = reader.readLine();
+        assertEquals("is a test", line);
+        reader.close();
+    }
+}
